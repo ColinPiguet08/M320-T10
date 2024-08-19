22fe52e71a6ef82b7290276917a2b31b229e539e
KennethTBZ <kenneth.dang@edu.tbz.ch>
2024-08-19 16:17:10 +0200

    Finish repetition m319 task

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+H
+repetition/319/Main.java,a\2\a24008364c09ce10b66df7eb97155da7fcb511df
+N
+repetition/319/GuessLogic.java,4\9\496c48d61929992024888815213f36ec6fea2c4b
---
+++ repetition/319/GuessLogic.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessLogic {
+    private void printWelcomeText() {
+        System.out.println("Welcome to guess the number.");
+    }
+
+    public void handleUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int answer = random.nextInt(11);
+        int guess;
+        int tries = 10;
+
+        printWelcomeText();
+        
+        while (tries >= 0) {
+            System.out.println("You have " + tries + " more tries.");
+            System.out.println("Guess a number between 0 and 10: ");
+
+            guess = scanner.nextInt();
+            scanner.nextLine();
+
+            if (checkGuess(guess, answer)) {
+                break;
+            }
+
+            tries--;
+
+            if (tries == 0) {
+                System.out.println("You are out of tries. The correct answer was " + answer);
+            }
+        }
+        scanner.close();
+    }
+
+    private boolean checkGuess(int guess, int answer) {
+        if (guess == answer) {
+            System.out.println("You have won.");
+            return true;
+        } else {
+            System.out.println("Your guess was wrong.");
+            return false;
+        }
+    }
+}
